da6285365096b95a44d9c71ec9861b690020dba4
svoekino <svoekino@bk.ru>
2021-10-21 08:47:06 +0300

    Add new dir for screenshots

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+/*jslint node: true */
+/*jshint esversion: 6 */
+/* jshint browser: true */
+
+// document.addEventListener('DOMContentLoaded', function() {
+
+console.log('sffddfsdfdssf');
+
+
+
+
+
+
+// });
